e0b2e75385f25a4d3b8559f45617c280559a33e5
Anastasiia <144330798+NastyaDudk@users.noreply.github.com>
2024-02-07 17:41:29 +0400

    ok

--- src/main.js
+++ src/main.js
@@ -4,108 +4,211 @@ import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-document.addEventListener('DOMContentLoaded', () => {
-  const formElem = document.querySelector('.form');
-  const galleryEl = document.querySelector('.gallery-el');
-  const loaderElem = document.querySelector('.loader');
-  const loadMoreBtn = document.querySelector('.load-more-btn');
-  let page = 1;
-  let query = '';
+const params = {
+  formSearch: document.querySelector('.form'),
+  imageList: document.querySelector('.gallery'),
+  preload: document.querySelector('.loader'),
+  nextBtn: document.querySelector('.load-more-btn'),
+};
 
-  hideLoader();
-  loadMoreBtn.style.display = 'none';
+const hiddenClass = 'is-hidden';
+let page = 0;
+let searchValue = '';
 
-  const lightbox = new SimpleLightbox('.gallery a', {
-    captionDelay: 250,
-  });
+const gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
-  formElem.addEventListener('submit', onSubmit);
-  loadMoreBtn.addEventListener('click', loadMoreImages);
+params.formSearch.addEventListener('submit', handleSearch);
 
-  async function onSubmit(e) {
-    e.preventDefault();
-    showLoader();
+async function handleSearch(event) {
+  event.preventDefault();
 
-    const value = formElem.querySelector('.input').value;
-    try {
-      const data = await getPhotoBySearch(value);
-      if (data.hits.length === 0) {
-        throw new Error('No images found');
-      }
-      query = value;
-      page = 1;
-      renderImages(data.hits);
-      loadMoreBtn.style.display = 'block';
-    } catch (error) {
-      renderError(error);
-    } finally {
-      hideLoader();
+  const searchQuery = event.currentTarget.elements.input.value.trim();
+  const form = event.currentTarget;
+
+  searchValue = searchQuery;
+  page = 1;
+
+  params.nextBtn.classList.add(hiddenClass);
+  params.imageList.innerHTML = '';
+
+  if (!searchQuery) {
+    iziToast.show({
+      title: '❕',
+      theme: 'light',
+      message: `Please, fill in the search field`,
+      messageSize: '20px',
+      messageColor: '#808080',
+      backgroundColor: '#e7fc44',
+      position: 'topRight',
+      timeout: 3000,
+    });
+    return;
+  }
+
+  params.preload.classList.remove(hiddenClass);
+
+  try {
+    const response = await fetchImages();
+    if (response.hits.length === 0) {
+      iziToast.show({
+        iconUrl: icon,
+        theme: 'dark',
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+        messageSize: '16px',
+        messageColor: 'white',
+        backgroundColor: '#EF4040',
+        position: 'topRight',
+        timeout: 5000,
+      });
+      form.reset();
+      return;
     }
 
-    formElem.reset();
+    params.imageList.innerHTML = createMarkup(response.hits);
+    gallery.refresh();
+
+    if (response.hits.length >= 40) {
+      addNextPageBtn();
+    }
+
+    scrollBy();
+    form.reset();
+  } catch (err) {
+    handleError(err);
+  } finally {
+    params.preload.classList.add(hiddenClass);
   }
+}
 
-  function getPhotoBySearch(searchValue) {
-    const BASE_URL = 'https://pixabay.com/api/';
-    const KEY = '42153847-0f7baac2d7b2e92d7ce6bbe8e';
-    const Query = `?key=${KEY}&q=${searchValue}`;
-    const params =
-      '&image_type=photo&orientation=horizontal&safesearch=true&per_page=15';
-    const url = BASE_URL + Query + params;
+async function fetchImages() {
+  const BASE_URL = 'https://pixabay.com/api';
+  const searchParams = new URLSearchParams({
+    key: '41861239-c6b09579488337e808a164f07',
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+    per_page: 40,
+  });
+  const res = await axios.get(
+    `${BASE_URL}/?${searchParams}&q=${searchValue}&page=${page}`
+  );
+  return res.data;
+}
 
-    return axios.get(url).then(res => {
-      return res.data;
-    });
-  }
-
-  function renderImages(array) {
-    const markup = array
-      .map(
-        ({
-          largeImageURL,
-          webformatURL,
-          tags,
-          likes,
-          views,
-          comments,
-          downloads,
-        }) => {
-          return `
-        <div class="gallery">
-          <a href="${largeImageURL}">
-            <img src="${webformatURL}" alt="${tags}" title="${tags}" width="360" height="300" />
-            <ul class="info-cards-container">
-              <li class="info-cards-elements">likes<span>${likes}</span></li>
-              <li class="info-cards-elements">views<span>${views}</span></li>
-              <li class="info-cards-elements">comments<span>${comments}</span></li>
-              <li class="info-cards-elements">downloads<span>${downloads}</span></li>
-            </ul>
-          </a>
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) =>
+        `<li class="gallery-item">
+      <a class="gallery-link" href="${largeImageURL}">
+        <img class="gallery-image" src="${webformatURL}" alt="${tags}" />
+      </a>
+      <div class="container-additional-info">
+        <div class="container-descr-inner">
+          <p class="description">Likes</p>
+          <span class="description-value">${likes}</span>
         </div>
-      `;
-        }
-      )
-      .join('');
+        <div class="container-descr-inner">
+          <p class="description">Views</p>
+          <span class="description-value">${views}</span>
+        </div>
+        <div class="container-descr-inner">
+          <p class="description">Comments</p>
+          <span class="description-value">${comments}</span>
+        </div>
+        <div class="container-descr-inner">
+          <p class="description">Downloads</p>
+          <span class="description-value">${downloads}</span>
+        </div>
+      </div>
+    </li>`
+    )
+    .join('');
+}
 
-    galleryEl.insertAdjacentHTML('beforeend', markup);
-    lightbox.refresh();
-  }
+function handleError(err) {
+  console.error(err);
+  params.imageList.innerHTML = '';
+  iziToast.show({
+    iconUrl: icon,
+    theme: 'dark',
+    message: err.stack,
+    messageSize: '16px',
+    messageColor: 'white',
+    backgroundColor: '#EF4040',
+    position: 'topRight',
+    timeout: 5000,
+  });
 
-  function renderError(error) {
-    galleryEl.innerHTML = '';
-    iziToast.show({
-      message: `❌ "${error.message}". Please try again!`,
-      color: 'red',
-      position: 'topRight',
-      maxWidth: '400px',
-    });
-  }
+  removeNextPageBtn();
+}
 
-  function showLoader() {
-    loaderElem.style.display = 'block';
-  }
+async function nextPage() {
+  params.preload.classList.remove(hiddenClass);
+  removeNextPageBtn();
+  page += 1;
 
-  function hideLoader() {
-    loaderElem.style.display = 'none';
+  try {
+    const res = await fetchImages();
+
+    if (page * 40 >= res.totalHits) {
+      iziToast.show({
+        title: '❕',
+        theme: 'dark',
+        message: "We're sorry, but you've reached the end of search results.",
+        messageSize: '16px',
+        messageColor: 'white',
+        backgroundColor: '#4e75ff',
+        position: 'topRight',
+        timeout: 5000,
+      });
+      params.imageList.innerHTML += createMarkup(res.hits);
+      gallery.refresh();
+      removeNextPageBtn();
+
+      scrollBy();
+
+      return;
+    }
+
+    params.imageList.innerHTML += createMarkup(res.hits);
+    gallery.refresh();
+
+    scrollBy();
+
+    addNextPageBtn();
+  } catch (err) {
+    handleError(err);
+  } finally {
+    params.preload.classList.add(hiddenClass);
   }
-});
+}
+
+function scrollBy() {
+  window.scrollBy({
+    top: 640,
+    behavior: 'smooth',
+  });
+}
+
+function addNextPageBtn() {
+  params.nextBtn.classList.remove(hiddenClass);
+  params.nextBtn.addEventListener('click', nextPage);
+}
+
+function removeNextPageBtn() {
+  params.nextBtn.classList.add(hiddenClass);
+  params.nextBtn.removeEventListener('click', nextPage);
+}
