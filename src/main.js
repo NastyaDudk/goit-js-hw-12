de9a324399a0caf7f1e57dc49a678f98c7f95d7f
Anastasiia <144330798+NastyaDudk@users.noreply.github.com>
2024-02-11 23:24:20 +0400

    ok

--- src/main.js
+++ src/main.js
@@ -1,132 +1,86 @@
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
-import Axios from 'axios';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import ImagesApiService from './images-service';
 
-document.addEventListener('DOMContentLoaded', () => {
-  const formElem = document.querySelector('.form');
-  const galleryEl = document.querySelector('.gallery-el');
-  const loaderElem = document.querySelector('.loader');
-  const loadMoreBtn = document.querySelector('.load-more');
+const imageApiService = new ImagesApiService();
+const form = document.querySelector('form#search-form');
+const gallery = document.querySelector('div.gallery');
+const loadMoreBtn = document.querySelector('button.load-more');
 
-  hideLoader();
-  hideLoadMoreButton();
+form.addEventListener('submit', onSubmit);
+loadMoreBtn.addEventListener('click', onLoadMore);
 
-  const lightbox = new SimpleLightbox('.gallery a', {
-    captionDelay: 250,
-  });
-
-  formElem.addEventListener('submit', onSubmit);
-  loadMoreBtn.addEventListener('click', onLoadMore);
-
-  let searchValue = '';
-  let currentPage = 1;
-
-  function onSubmit(e) {
-    e.preventDefault();
-    showLoader();
-
-    searchValue = formElem.querySelector('.input').value;
-    currentPage = 1;
-    getPhotoBySearch(searchValue, currentPage)
+function onSubmit(e) {
+  e.preventDefault();
+  loadMoreBtn.classList.add('is-hidden');
+  gallery.innerHTML = '';
+  imageApiService.query = e.currentTarget.elements.searchQuery.value.trim();
+  imageApiService.resetPage();
+  if (imageApiService.query === '') {
+    Notify.info('Please enter your search query!');
+    return;
+  } else {
+    imageApiService
+      .getImage()
       .then(data => {
-        renderImages(data.hits);
-        showLoadMoreButton();
-      })
-      .catch(error => {
-        renderError(error);
-        hideLoadMoreButton();
-      })
-      .finally(() => {
-        hideLoader();
-      });
-
-    formElem.reset();
-  }
-
-  function onLoadMore() {
-    showLoader();
-    currentPage++;
-    getPhotoBySearch(searchValue, currentPage)
-      .then(data => {
-        renderImages(data.hits);
-        showLoadMoreButton();
-      })
-      .catch(error => {
-        renderError(error);
-        hideLoadMoreButton();
-      })
-      .finally(() => {
-        hideLoader();
-      });
-  }
-
-  function getPhotoBySearch(searchValue, page) {
-    const BASE_URL = 'https://pixabay.com/api/';
-    const KEY = '42153847-0f7baac2d7b2e92d7ce6bbe8e';
-    const Query = `?key=${KEY}&q=${searchValue}`;
-    const params =
-      '&image_type=photo&orientation=horizontal&safesearch=true&per_page=20';
-    const url = BASE_URL + Query + params + `&page=${page}`;
-
-    return fetch(url)
-      .then(res => res.json())
-      .then(data => {
-        if (data.total === 0) {
-          throw new Error('No images found');
+        let queriesArray = data.hits;
+        if (queriesArray.length === 0) {
+          Notify.failure(
+            'Sorry, there are no images matching your search query. Please try again.'
+          );
+        } else if (queriesArray.length < 40) {
+          renderImages(queriesArray);
+          loadMoreBtn.classList.add('is-hidden');
+          Notify.success(`Hooray! We found ${data.totalHits} images.`);
+          // Notify.info(
+          //   "We're sorry, but you've reached the end of search results."
+          // );
+        } else {
+          renderImages(queriesArray);
+          Notify.success(`Hooray! We found ${data.totalHits} images.`);
+          loadMoreBtn.classList.remove('is-hidden');
         }
-        return data;
+      })
+      .catch(error => {
+        Notify.info(
+          "We're sorry, but you've reached the end of search results."
+        );
+        console.log(error);
       });
   }
+}
 
-  function renderImages(array) {
-    let markup = '';
-    array.forEach(
-      ({
-        largeImageURL,
-        webformatURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) => {
-        markup += `
-          <div class="gallery">
-            <a href="${largeImageURL}">
-              <img src="${webformatURL}" alt="${tags}" title="${tags}" width="360" height="300" />
-              <ul class="info-cards-container">
-                <li class="info-cards-elements">likes<span>${likes}</span></li>
-                <li class="info-cards-elements">views<span>${views}</span></li>
-                <li class="info-cards-elements">comments<span>${comments}</span></li>
-                <li class="info-cards-elements">downloads<span>${downloads}</span></li>
-              </ul>
-            </a>
-          </div>
-        `;
-      }
-    );
+function onLoadMore() {
+  imageApiService.getImage().then(data => {
+    let queriesArray = data.hits;
+    renderImages(queriesArray);
+    if (queriesArray.length < 40) {
+      loadMoreBtn.classList.add('is-hidden');
+      Notify.info("We're sorry, but you've reached the end of search results.");
+    }
+  });
+}
 
-    galleryEl.insertAdjacentHTML('beforeend', markup);
-    lightbox.refresh();
-  }
-
-  function renderError(error) {
-    galleryEl.innerHTML = '';
-    iziToast.show({
-      message: `❌ "${error.message}". Please try again!`,
-      color: 'red',
-      position: 'topRight',
-      maxWidth: '400px',
-    });
-  }
-
-  function showLoader() {
-    loaderElem.style.display = 'block';
-  }
-
-  function hideLoader() {
-    loaderElem.style.display = 'none';
-  }
-});
+function renderImages(queriesArray) {
+  const markup = queriesArray
+    .map(item => {
+      return `<div class="photo-card">
+  <div class="thumb"><img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" /></div>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b><span>${item.likes}</span>
+    </p>
+    <p class="info-item">
+      <b>Views</b><span>${item.views}</span>
+    </p>
+    <p class="info-item">
+      <b>Comments</b><span>${item.comments}</span>
+    </p>
+    <p class="info-item">
+      <b>Downloads</b><span>${item.downloads}</span>
+    </p>
+  </div>
+</div>`;
+    })
+    .join('');
+  gallery.insertAdjacentHTML('beforeend', markup);
+}
