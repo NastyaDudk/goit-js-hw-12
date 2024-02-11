ec72c284ddaba7d68cd88e6ec2cd222734b7fe34
Anastasiia <144330798+NastyaDudk@users.noreply.github.com>
2024-02-11 17:42:53 +0400

    ok

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import errorIcon from '../src/img/bi_x-octagon.svg';
+
 import axios from 'axios';
 
 const searchForm = document.querySelector('.js-search-form');
