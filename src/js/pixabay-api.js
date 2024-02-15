be8d9b5fd1f2241c83d758d51293367cdf056575
Anastasiia <144330798+NastyaDudk@users.noreply.github.com>
2024-02-15 10:57:38 -0500

    ok

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,66 @@
+import { refs } from './refs';
+import { loaderOn } from './loader';
+import { loaderOff } from './loader';
+import { makeGalleryItem } from './render-functions';
+import { onError } from './iziToasts';
+import axios from 'axios';
+import { MESSAGE } from './iziToasts';
+import { totalHits } from './render-functions';
+import { LIMIT } from './iziToasts';
+import { scroll } from './render-functions';
+
+const perPage = 15;
+let page = 1;
+let userSearch;
+
+export async function onFormSubmit(event) {
+  event.preventDefault();
+  refs.btnLoad.classList.add('hidden');
+
+  refs.galleryList.innerHTML = '';
+  page = 1;
+  userSearch = event.target.elements.input.value.trim();
+  try {
+    const response = await fetchImg(userSearch);
+    const item = makeGalleryItem(response);
+    return item;
+  } catch (error) {
+    onError(MESSAGE);
+  } finally {
+    loaderOff();
+    refs.form.reset();
+  }
+}
+
+export async function fetchImg(input) {
+  loaderOn();
+  const API_KEY = '42220995-e7901b62efa710cae16c4a0a7';
+  axios.defaults.baseURL = 'https://pixabay.com/api/';
+  const parameters = `q=${input}&image_type=photo&orientation=horizontal&safesearch=true`;
+  const URL = `?key=${API_KEY}&${parameters}`;
+
+  const config = {
+    params: {
+      per_page: perPage,
+      page: page,
+    },
+  };
+
+  const response = await axios.get(URL, config);
+  return response.data;
+}
+
+export async function onLoadClick() {
+  page += 1;
+  const totalPages = Math.ceil(totalHits / perPage);
+  if (page > totalPages) {
+    refs.btnLoad.classList.add('hidden');
+
+    onError(LIMIT);
+  } else {
+    const response = await fetchImg(userSearch);
+    const item = makeGalleryItem(response);
+    loaderOff();
+    return item;
+  }
+}
---
+++ src/js/render-functions.js
@@ -0,0 +1,60 @@
+import { refs } from './refs';
+import { onError } from './iziToasts';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { MESSAGE } from './iziToasts';
+import { LIMIT } from './iziToasts';
+
+export let totalHits;
+
+export function makeGalleryItem(response) {
+  const result = response.hits.map(makeMarcup).join('');
+  totalHits = response.totalHits;
+
+  if (response.hits.length) {
+    refs.galleryList.insertAdjacentHTML('beforeend', result);
+    refs.btnLoad.classList.remove('hidden');
+
+    let lightbox = new SimpleLightbox('.gallery a', {
+      captionsData: 'alt',
+      captionPosition: 'bottom',
+      captionDelay: 250,
+    });
+
+    lightbox.refresh();
+  } else {
+    onError(MESSAGE);
+  }
+}
+
+export function makeMarcup(image) {
+  return `<li class="gallery-item">
+        <a href="${image.largeImageURL}" class="gallery-link">
+          <img
+            src="${image.webformatURL}"
+            class="gallery-image"
+            alt="${image.tags}"
+          />
+        </a>
+        <div class="modat-text">
+        
+            <div class="modal-element"><p>Likes</p><span>${image.likes}</span></div>
+            <div class="modal-element"><p>Views</p><span>${image.views}</span></div>
+            <div class="modal-element"><p>Comments</p><span>${image.comments}</span></div>
+            <div class="modal-element"><p>Downloads</p><span>${image.downloads}</span></div>
+        </div>
+      </li>`;
+}
+
+export function scroll() {
+  const galleryItem = document.querySelector('.gallery-item');
+  if (galleryItem) {
+    let rect = refs.galleryItem.getBoundingClientRect();
+    console.log(rect.height);
+    window.scrollBy({
+      top: rect.height * 2,
+      behavior: 'smooth',
+    });
+  }
+}
