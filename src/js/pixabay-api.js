8e58e003b7481a7d4888b703b01fe0051efbbf62
Anastasiia <144330798+NastyaDudk@users.noreply.github.com>
2024-02-19 12:50:13 -0500

    ok

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+let totalHits = 0;
+
 export async function searchImages(query, page = 1) {
   const url = `https://pixabay.com/api/?key=${apiKey}&q=${encodeURIComponent(
     query
@@ -18,23 +20,6 @@ export async function searchImages(query, page = 1) {
 export function displayImages(images) {
   galleryContainer.innerHTML = '';
   appendImages(images);
-}
-
-let totalHits = 0;
-
-export async function searchImages(query, page = 1) {
-  const url = `https://pixabay.com/api/?key=${apiKey}&q=${encodeURIComponent(
-    query
-  )}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=15`;
-  try {
-    const response = await axios.get(url);
-    totalHits = response.data.totalHits;
-    return response.data.hits;
-  } catch (error) {
-    console.error('Error fetching images:', error);
-    toastError('Failed to fetch images.');
-    throw error;
-  }
 }
 
 export function appendImages(images) {
