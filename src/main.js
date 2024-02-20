ae583a9a2d194b658a8eccf15843acb66af1a430
Anastasiia <144330798+NastyaDudk@users.noreply.github.com>
2024-02-19 23:08:13 -0500

    ok

--- src/main.js
+++ src/main.js
@@ -141,33 +141,6 @@ function displayImages(images) {
   appendImages(images);
 }
 
-function appendImages(images) {
-  const fragment = document.createDocumentFragment();
-  images.forEach(image => {
-    const {
-      largeImageURL,
-      webformatURL,
-      tags,
-      likes,
-      views,
-      comments,
-      downloads,
-    } = image;
-    const imageCard = document.createElement('div');
-    imageCard.classList.add('image-card');
-    imageCard.innerHTML = `
-            <a href="${largeImageURL}" data-lightbox="image-set" data-title="${tags}">
-                <img src="${webformatURL}" alt="${tags}">
-                <div class="info">Likes: ${likes}, Views: ${views}, Comments: ${comments}, Downloads: ${downloads}</div>
-            </a>
-        `;
-    fragment.appendChild(imageCard);
-  });
-  galleryContainer.appendChild(fragment);
-
-  currentImagesCount += images.length;
-}
-
 let lightbox = null;
 
 function initializeLightbox() {
